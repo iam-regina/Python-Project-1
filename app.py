62dbdeddcb4d605f0b70008cd9fdc0db9e2f3601
PinkLasagna1 <69775722+PinkLasagna1@users.noreply.github.com>
2020-09-28 09:31:50 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+# Friend Simulator
+name = input("Hi there! What's your name?")
+print("I am Tim, nice to meet you " + name)
+feeling=input("How do you feel right now?")
+  if feeling == "sad":
+    feelings_list.append("sad")
+    print("aww... don't worry. You get better over time :D ")
+  if feeling == "happy":
+    feelings_list.append("happy")
+    print("Great! Now continue showing the world that smile of yours :P ")
+  if feeling == "tired":
+    feelings_list.append("tired")
+    print("Maybe it is time for you to rest, go on. ")
+    
+       
